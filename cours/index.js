fe188e00a94dc0558d22581181d20d3c3d22aef4
PUX Remy (SNCF RESEAU / DIRECTION GENERALE ILE DE FRANCE / DZP IDF BHR PRG) <remy80.dev@gmail.com>
2023-06-01 16:01:11 +0200

    essai commit

---
+++ cours/index.js
@@ -0,0 +1,12 @@
+//SELECTEURS
+// document.querySelector('body').style.background = "blue";
+// const baliseHTML = document.querySelector('h4');
+// baliseHTML.style.background = "red";
+
+//Click event
+const questionContainer = document.querySelector(".click-event");
+
+// questionContainer.addEventListener("click", () => {
+//     console.log("click!!");
+// })
+// essai github
